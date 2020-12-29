1d5bd7a503b9ad5580abbb0ef652f362e6eedf8f
MarioJean <42340502+MarioJean@users.noreply.github.com>
2020-12-29 04:09:34 -0400

    blog models

--- avele/settings.py
+++ avele/settings.py
@@ -66,8 +66,11 @@ WSGI_APPLICATION = 'avele.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'aveleDB',
+        'USER': 'postgres',
+        'PASSWORD': '14203',
+        'HOST': 'localhost'
     }
 }
 
