8455f55389d8f8fb10fc22e4516010fa93a9e572
MarioJean <42340502+MarioJean@users.noreply.github.com>
2021-01-17 13:41:20 +0300

    blog page and pagination

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='blog'),
-    path('<int:blog_id>', views.blogpost, name='blogpost'),
+    path('<int:blogpost_id>', views.blogpost, name='blogpost'),
     path('search', views.search, name='search'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Blogpost
 
 def index(request):
-    blog = Blogpost.objects.all()
+    blog = Blogpost.objects.order_by('-list_date').filter(is_published=True)
+    
+    paginator = Paginator(blog, 4)
+    page = request.GET.get('page')
+    paged_blog = paginator.get_page(page)
     
     context = {
-        'blog': blog
+        'blog': paged_blog
     }
     
     return render(request, 'blog/blog.html', context)
     
-def blogpost(request):
+def blogpost(request, blogpost_id):
     return render(request, 'blog/blogpost.html')
 
 def search(request):
