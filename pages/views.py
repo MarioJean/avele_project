4598134fb3404e2c08b51bcdabc607e23d1e4aa4
MarioJean <42340502+MarioJean@users.noreply.github.com>
2021-02-05 04:20:36 -0500

    Index Shop

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from blog.models import Blogpost
 
 def index(request):
-    return render(request, 'pages/index.html')
+    blog = Blogpost.objects.order_by('-list_date').filter(is_published=True)[:3]
+    
+    context = {
+        'blog': blog
+    }
+    
+    return render(request, 'pages/index.html', context)
 
 def store(request):
     return render(request, 'pages/store.html')
