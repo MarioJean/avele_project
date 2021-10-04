ff6c5ceaee98ee11a913e9ece2c9fa5c45ba514f
MarioJean <42340502+MarioJean@users.noreply.github.com>
2021-10-04 18:54:03 +0300

    Ckeditor Install

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from datetime import datetime
+from ckeditor.fields import RichTextField
 
 class Blogpost(models.Model):
     title = models.CharField(max_length=200)
     category = models.CharField(max_length=20, choices=[('health', 'health'), ('fitness', 'fitness'),
                                 ('lifestyle', 'lifestyle'), ('workout', 'workout')])
-    article = models.TextField()
+    article = RichTextField(blank=True, null=True)
     highlight_text = models.TextField(default="Keep Moving Forward")
     photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
     read_time = models.CharField(max_length=10, default="5 min")
